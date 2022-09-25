f6d4a49e88cf6dda613d1b71b60b53d3df4e7a5a
hujinfeng <pbfoxhu@gmail.com>
2022-09-25 11:42:30 +0800

    test

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -8,9 +8,14 @@ import (
 func TestGraph(t *testing.T) {
 	a := NewNode("1_0_0", nil)
 	b := NewNode("1_0_1", nil)
-	//e := NewEdge(a, b, 0)
 	graph := NewGraph(nil, nil)
 	graph.AddEdgeNodes(a, b, 0)
+	c := NewNode("1_0_2", nil)
+	graph.AddEdgeNodes(b, c, 0)
+	d := NewNode("1_0_3", nil)
+	graph.AddEdgeNodes(c, d, 0)
+	e := NewNode("1_0_4", nil)
+	graph.AddEdgeNodes(d, e, 0)
 
 	fmt.Println(graph)
 }
